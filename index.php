eaf50af0a56251a249cdcc072dd77db0c2c9651d
Rob Treacy <email@roberttreacy.com>
2016-10-16 15:22:16 +0100

    Achieved working prototype

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'bootstrap.php';
+
+use OAuth\OAuth2\Service\Instagram;
+use OAuth\Common\Storage\Session;
+use OAuth\Common\Consumer\Credentials;
+
+
+// Setup the credentials for the requests
+$credentials = new Credentials(
+    $servicesCredentials['instagram']['key'],
+    $servicesCredentials['instagram']['secret'],
+    $currentUri->getAbsoluteUri()
+);
+
+$scopes = array('basic', 'comments', 'relationships', 'likes');
+
+// Instantiate the Instagram service using the credentials, http client and storage mechanism for the token
+/** @var $instagramService Instagram */
+$instagramService = $serviceFactory->createService('instagram', $credentials, $storage, $scopes);
+
+$data = json_decode($instagramService->request('/users/self/media/recent'));
+
+foreach($data->data as $image) {
+    printf('<img src="%s" />', $image->images->low_resolution->url);
+}
