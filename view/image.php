2f6a0e7d797168ce3ab50456246700ce9625bc8d
Rob Treacy <email@roberttreacy.com>
2017-08-13 17:14:59 +0100

    Implemented Angular to replace jQuery Removed lightbox Added some animation for new image screen.

--- view/image.php
+++
@@ -1,7 +0,0 @@
-<li>
-    <?php printf('<img src="%s" />', $image->images->standard_resolution->url); ?>
-    <p class="qr">
-        <?php echo $qrCode->image("image alt", ['class' => 'qr-code-img']); ?>
-        Scan the code to see your photo on instagram
-    </p>
-</li>
